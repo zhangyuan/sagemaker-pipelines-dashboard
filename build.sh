b4bab805dc9d57c64570c191076588b3970f9f8d
Yuan Zhang <zhangyuan@users.noreply.github.com>
2023-04-25 11:48:06 +0800

    build for multiple platforms

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -euo pipefail
+
+GOOS=windows GOARCH=amd64 go build -o bin/sagemaker-pipelines-dashboard-amd64.exe
+GOOS=windows GOARCH=386 go build -o bin/sagemaker-pipelines-dashboard-386.exe
+GOOS=darwin GOARCH=amd64 go build -o bin/sagemaker-pipelines-dashboard-amd64-darwin
+GOOS=darwin GOARCH=386 go build -o bin/sagemaker-pipelines-dashboard-386-darwin
+GOOS=linux GOARCH=amd6 go build -o bin/sagemaker-pipelines-dashboard-amd64-linux
+GOOS=linux GOARCH=386 go build -o bin/sagemaker-pipelines-dashboard-386-linux
