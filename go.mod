8d6f6cd1f3c3be68efe57796dab57cb8cb902df5
Yuan Zhang <zhangyuan@users.noreply.github.com>
2023-04-25 11:20:35 +0800

    display sagemaker pipeline execution status

---
+++ go.mod
@@ -0,0 +1,21 @@
+module sagemaker-pipelines-dashboard
+
+go 1.18
+
+require (
+	github.com/aws/aws-sdk-go v1.44.247
+	github.com/jedib0t/go-pretty v4.3.0+incompatible
+)
+
+require (
+	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2 // indirect
+	github.com/go-openapi/errors v0.20.3 // indirect
+	github.com/go-openapi/strfmt v0.21.7 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/oklog/ulid v1.3.1 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	go.mongodb.org/mongo-driver v1.11.3 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sagemaker"
+	"github.com/jedib0t/go-pretty/table"
+	"github.com/jedib0t/go-pretty/text"
+)
+
+func main() {
+	if err := invoke(); err != nil {
+		log.Fatalln(err)
+	}
+}
+
+type Pipelines struct {
+	name                        string
+	status                      string
+	lastModifiedTime            *time.Time
+	lastPipelineExecutionStatus string
+}
+
+func GetPipelines() (*[]Pipelines, error) {
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+	svc := sagemaker.New(sess)
+
+	listPipelinesOutput, err := svc.ListPipelines(nil)
+	if err != nil {
+		return nil, err
+	}
+
+	pipelines := []Pipelines{}
+
+	for _, pipline := range listPipelinesOutput.PipelineSummaries {
+		name := pipline.PipelineName
+		describePipelineInput := sagemaker.DescribePipelineInput{
+			PipelineName: name,
+		}
+		output, err := svc.DescribePipeline(&describePipelineInput)
+		if err != nil {
+			return nil, err
+		}
+
+		pipelineName := *output.PipelineName
+		pipelineStatus := *output.PipelineStatus
+
+		pipeline := Pipelines{
+			name:             pipelineName,
+			status:           pipelineStatus,
+			lastModifiedTime: output.LastModifiedTime,
+		}
+
+		sortBy := "CreationTime"
+		sortOrder := "Descending"
+		maxResults := int64(1)
+		listPipelineExecutionsInput := &sagemaker.ListPipelineExecutionsInput{
+			PipelineName: &pipelineName,
+			SortBy:       &sortBy,
+			SortOrder:    &sortOrder,
+			MaxResults:   &maxResults,
+		}
+
+		listPipelineExecutionsOutput, err := svc.ListPipelineExecutions(listPipelineExecutionsInput)
+
+		if err != nil {
+			return nil, err
+		}
+
+		pipelineExecutionSummaries := listPipelineExecutionsOutput.PipelineExecutionSummaries
+
+		if len(pipelineExecutionSummaries) > 0 {
+			pipelineExecutionSummary := listPipelineExecutionsOutput.PipelineExecutionSummaries[0]
+			pipeline.lastPipelineExecutionStatus = *pipelineExecutionSummary.PipelineExecutionStatus
+		}
+
+		pipelines = append(pipelines, pipeline)
+	}
+
+	return &pipelines, nil
+}
+
+func invoke() error {
+	pipelines, err := GetPipelines()
+	if err != nil {
+		return err
+	}
+
+	t := table.NewWriter()
+	t.SetOutputMirror(os.Stdout)
+
+	t.AppendHeader(table.Row{"Name", "PipelineStatus", "LastModifiedTime", "ExecutionStatus"})
+
+	rows := []table.Row{}
+
+	for _, pipeline := range *pipelines {
+		var executionStatus string
+		text.BgBlue.Sprint(pipeline.status)
+		if pipeline.lastPipelineExecutionStatus == "Succeeded" {
+			executionStatus = text.BgGreen.Sprint(pipeline.lastPipelineExecutionStatus)
+		} else if pipeline.lastPipelineExecutionStatus == "Executing" {
+			executionStatus = text.BgBlue.Sprint(pipeline.lastPipelineExecutionStatus)
+		} else if pipeline.lastPipelineExecutionStatus == "Failed" {
+			executionStatus = text.BgRed.Sprint(pipeline.lastPipelineExecutionStatus)
+		}
+		rows = append(rows, table.Row{pipeline.name, pipeline.status, pipeline.lastModifiedTime, executionStatus})
+	}
+
+	t.AppendRows(rows)
+	t.Render()
+
+	return nil
+}
